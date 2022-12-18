be75554385f255192657c3ccad68f842f54d9420
Maciej Handke <maciejhandke11@gmail.com>
2022-12-18 22:21:16 +0100

    Add rendering error for no results

--- js/views/resultsView.js
+++ js/views/resultsView.js
@@ -2,7 +2,7 @@ import View from "./View";
 
 class resultsView extends View {
   _parentElement = document.querySelector("#results-container");
-
+  _errorMessage = "No recipes found";
   _generateMarkup() {
     console.log(this._data);
     return this._data.map(this._generateMarkupResult).join("");
