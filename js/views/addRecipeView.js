05f3e5560f445860c82cf5d5cd3e59ee14b04f08
Maciej Handke <maciejhandke11@gmail.com>
2023-01-03 22:42:51 +0100

    Add closing form method

--- js/views/addRecipeView.js
+++ js/views/addRecipeView.js
@@ -2,13 +2,20 @@ import View from "./View";
 
 class addRecipeView extends View {
   _parentElement = document.querySelector(".upload-form");
+  _overlay = document.querySelector("#overlay");
+  _addRecipeModal = document.querySelector(".add-recipe-modal");
+  _message = "Recipe was successfully uploaded";
+
+  toggleWindow() {
+    this._overlay.classList.toggle("hidden");
+    this._addRecipeModal.classList.toggle("hidden");
+  }
 
   addHandlerUpload(handler) {
     this._parentElement.addEventListener("submit", function (e) {
       e.preventDefault();
       const dataArray = [...new FormData(this)];
       const data = Object.fromEntries(dataArray);
-      console.log("Submmitted");
       handler(data);
     });
   }
